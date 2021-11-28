d5df37044048cd718def8a7c57274bfb9bb8a6e8
Yaroslav Zhvalevskiy <yaroslav.zhvalevskiy@gmail.com>
2021-11-28 14:08:57 +0300

    classwork part1

--- src/config/.mocharc.js
+++ src/config/.mocharc.js
@@ -1,7 +1,17 @@
+// const path = require('path');
+// console.log(__dirname);
+// console.log(process.cwd());
+
 module.exports = {
+    color: true,
+    extension: ['js', 'cjs', 'mjs'],
+    parallel: false,
+    recursive: false,
     reporter: 'spec',
     retries: 1,
+    sort: false,
     spec: ['src/tests/**/*.js'], // the positional arguments!
+    // spec: [path.resolve(process.cwd(), `src/tests/**/*.js`)],
     timeout: '200000', // same as "timeout: '2s'"
     ui: 'bdd'
 };
--- src/tests/test.js
+++ src/tests/test.js
@@ -1,9 +1,31 @@
-var assert = require('assert');
+const puppeteer = require('puppeteer');
+const { expect } = require('chai');
 
-describe('Array', function () {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
-            assert.equal([1, 2, 3].indexOf(4), -1);
+let page, browser;
+
+describe('example spec', function () {
+    before(async () => {
+        browser = await puppeteer.launch({ headless: false });
+    })
+
+    after(() => browser.close());
+
+    beforeEach(async () => {
+        page = await browser.newPage();
+        await page.goto('https://pptr.dev/');
+    })
+
+    afterEach(async () => page.close());
+
+    it('second test', async function () {
+        await page.waitForResponse(resp => {
+            return resp.url() === 'https://img.shields.io/npm/v/puppeteer.svg' && resp.status() === 200;
         });
-    });
+        // await page.waitForTimeout(5000);
+        const title = (await page.$$('content-box h1'))[0];
+        const text = (await page.evaluate(element => element.textContent, title)).trim();
+        const text1 = (await title.evaluate((node => node.innerText))).trim();
+        expect(text).to.equal('Puppeteer');
+        expect(text1).to.equal('Puppeteer');
+    })
 });
